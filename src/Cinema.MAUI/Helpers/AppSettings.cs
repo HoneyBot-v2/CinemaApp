a1bb87a7fcd083a754cb66ffd5a0bf031ec6cd77
Jacob <jacob.rail49@gmail.com>
2025-12-20 12:06:39 -0700

    Introduce AppSettings to centralize API base URL

---
+++ src/Cinema.MAUI/Helpers/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.MAUI.Helpers;
+
+internal class AppSettings
+{
+    public static string ApiUrl => "https://localhost:5001/api";
+}
