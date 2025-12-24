b1278007d40027353d0bb5bccf076b24f46bba2d
Jacob <jacob.rail49@gmail.com>
2025-12-24 13:07:21 -0700

    fix(models): initialize User.Name property to prevent null reference issues

--- src/Cinema.API/Models/User.cs
+++ src/Cinema.API/Models/User.cs
@@ -3,7 +3,7 @@ namespace Cinema.API.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string Email { get; set; }
         public string Password { get; set; }
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
