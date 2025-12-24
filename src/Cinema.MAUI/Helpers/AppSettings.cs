ca975e547cbaf4ec15c3dc98f90317d1de14294a
Jacob <jacob.rail49@gmail.com>
2025-12-24 13:07:11 -0700

    fix(maui): fix 404 error by append trailing slashes to API URLs in AppSettings

--- src/Cinema.MAUI/Helpers/AppSettings.cs
+++ src/Cinema.MAUI/Helpers/AppSettings.cs
@@ -7,13 +7,13 @@ namespace Cinema.MAUI.Helpers;
 internal static class AppSettings
 {
 #if ANDROID
-    public const string HttpsApiUrl = "https://10.0.2.2:5001/api";
-    public const string HttpApiUrl = "http://10.0.2.2:5000/api";
+    public const string HttpsApiUrl = "https://10.0.2.2:5001/api/";
+    public const string HttpApiUrl = "http://10.0.2.2:5000/api/";
 #elif IOS
-    public const string HttpsApiUrl = "https://localhost:5001/api";
-    public const string HttpApiUrl = "http://localhost:5000/api";
+    public const string HttpsApiUrl = "https://localhost:5001/api/";
+    public const string HttpApiUrl = "http://localhost:5000/api/";
 #else
-    public const string HttpsApiUrl = "https://localhost:5001/api"; // Windows/macOS
-    public const string HttpApiUrl = "http://localhost:5000/api"; // Windows/macOS
+    public const string HttpsApiUrl = "https://localhost:5001/api/"; // Windows/macOS
+    public const string HttpApiUrl = "http://localhost:5000/api/"; // Windows/macOS
 #endif
 }
