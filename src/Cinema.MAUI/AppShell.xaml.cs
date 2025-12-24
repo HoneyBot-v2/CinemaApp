ddf18b537bb4f7222fe057d80a769e2abb7348f9
Jacob <jacob.rail49@gmail.com>
2025-12-24 12:17:26 -0700

    feat(maui): configure AppShell with TabBar and routes

--- src/Cinema.MAUI/AppShell.xaml.cs
+++ src/Cinema.MAUI/AppShell.xaml.cs
@@ -5,6 +5,17 @@
         public AppShell()
         {
             InitializeComponent();
+
+            // Register non-visual routes
+            Routing.RegisterRoute("registration", typeof(Pages.RegistrationPage));
+            Routing.RegisterRoute("login", typeof(Pages.LoginPage));
+
+            // Navigate to the Registration on startup as a modal
+            Dispatcher.DispatchAsync(async () =>
+            {
+                // Modal presentation defined on the RegistrationPage via Shell attributes
+                await Shell.Current.GoToAsync("registration");
+            });
         }
     }
 }
