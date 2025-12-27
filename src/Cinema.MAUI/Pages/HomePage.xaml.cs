659f0d17e2e4c1a2c98e9d8df805f6ad4cb1ad3c
Jacob <jacob.rail49@gmail.com>
2025-12-27 14:22:33 -0700

    feat(maui): display now playing and latest movies on home page

--- src/Cinema.MAUI/Pages/HomePage.xaml.cs
+++ src/Cinema.MAUI/Pages/HomePage.xaml.cs
@@ -11,12 +11,28 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
         Token token = PreferenceHelper.Load<Token>();
 		LblUserName.Text = token.UserName;
+
+		// Populate movie collection views
 		TrendingMovies();
+		NowPlayingMovies();
+		LatestMovies();
     }
 
 	private async void TrendingMovies()
 	{
         List<Movie> movies = await ApiService.GetMovies("trending");
 		CvTrending.ItemsSource = movies;
+	}
+
+	private async void NowPlayingMovies()
+	{
+		List<Movie> movies = await ApiService.GetMovies("nowplaying");
+		CvNowPlaying.ItemsSource = movies;
+    }
+
+	private async void LatestMovies()
+	{
+		List<Movie> movies = await ApiService.GetMovies("latest");
+		CvLatest.ItemsSource = movies;
 	}
 }
