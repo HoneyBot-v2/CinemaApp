566600719a2f04f984017adab91a431c738a90d5
Jacob <jacob.rail49@gmail.com>
2025-12-29 14:58:55 -0700

    feat(maui): add seat selection state to Seat model
    - Implement INotifyPropertyChanged for dynamic UI updates
    - Add IsSelected property with property change notifications
    - Add SeatIcon computed property for visual state management
    - Configure JSON property name mappings for API deserialization

--- src/Cinema.MAUI/Models/Seat.cs
+++ src/Cinema.MAUI/Models/Seat.cs
@@ -1,18 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Text.Json.Serialization;
 
 namespace Cinema.MAUI.Models;
 
-internal class Seat
+public class Seat : INotifyPropertyChanged
 {
+    // JSON properties
     [JsonPropertyName("id")]
     public required int Id { get; set; }
     [JsonPropertyName("row")]
     public required string Row { get; set; }
-    [JsonPropertyName("seat_number")]
+    [JsonPropertyName("seatNumber")]
     public required int SeatNumber { get; set; }
-    [JsonPropertyName("is_available")]
+    [JsonPropertyName("isAvailable")]
     public required bool IsAvailable { get; set; }
+
+    // Backing field and property
+    private bool _isSelected;
+    public bool IsSelected
+    {
+        get { return _isSelected; }
+        set {
+            if (_isSelected != value)
+            {
+                _isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+                OnPropertyChanged(nameof(SeatIcon));
+            }
+        }
+    }
+
+    public string SeatIcon
+    {
+        get
+        {
+            if (!IsAvailable)
+            {
+                return "seatunavailable_icon.png";
+            }
+            else if (IsSelected)
+            {
+                return "seatselected_icon.png";
+            }
+
+            return "seatavailable_icon.png";
+        }
+    }
+
+    // INotifyPropertyChanged implementation
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
